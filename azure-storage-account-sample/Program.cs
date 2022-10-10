96ff76aa44c03fe2c52575b19f928377edacd458
doniyorniazov <niazovd@gmail.com>
2022-10-10 11:24:40 +0500

    initialize proj

---
+++ azure-storage-account-sample/Program.cs
@@ -0,0 +1,26 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Specialized;
+
+namespace azure_storage_account_sample;
+class Program
+{
+    static  void Main(string[] args)
+    {
+        string connectionString = "DefaultEndpointsProtocol=https;AccountName=doniyorstorage;AccountKey=NxHytN2IYTCRaFwxhYjaEZT71N0yY2xm59gHk5BMLPvu/e7oME35hq2xxr705mcsBZLQd5m11owF+ASt4F6pxw==;EndpointSuffix=core.windows.net";
+
+        BlobServiceClient blobClient = new BlobServiceClient(connectionString);
+
+        string sourceContainer = "firstcontainer";
+        string destinationContainer = "thirdContainer";
+        string sourceFileName = "DNO00301.jpg";
+
+        string destionationFile = "Copied - DNO00301.jpg";
+        var sourceClient = new BlockBlobClient(connectionString, sourceContainer, sourceFileName);
+        var destinationClient = new BlockBlobClient(connectionString, destinationContainer, destionationFile);
+
+         destinationClient.StartCopyFromUri(sourceClient.Uri);
+
+        Console.WriteLine("Hello, World!");
+    }
+}
+
